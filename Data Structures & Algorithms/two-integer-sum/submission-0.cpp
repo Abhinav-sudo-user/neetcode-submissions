4fea6dc134f20b37a62d6df3bab7ff2b68e7f44f
Abhinav-sudo-user <91213881+Abhinav-sudo-user@users.noreply.github.com>
2026-07-13 18:01:04 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> num_to_index;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int c = target - nums[i];
+
+            if (num_to_index.count(c)) {
+                return {num_to_index[c], i};
+            }
+            num_to_index[nums[i]] = i;
+        }
+        return {};
+    }
+};
